0df09203d2d9826ee1f4653c845b50d96cf77b73
Thomas Davidson <tomd1969@gmail.com>
2023-02-06 16:27:36 -0500

    Added additional band routes

--- controllers/bands_controller.js
+++ controllers/bands_controller.js
@@ -36,4 +36,36 @@ bands.post('/', async(req, res) => {
     }
 })
 
+//UPDATE A BAND
+bands.put('/:id', async(req, res) => {
+    try {
+        const updatedBands = await Band.update(req.body, {
+            where: {
+                band_id: req.params.id
+            }
+        })
+        res.status(200).json({
+            message: `Successfully updated ${updatedBands} band(s)`
+        })
+    } catch (error) {
+        res.status(500).json(error)
+    }
+})
+
+//DELETE A BAND
+bands.delete('/:id', async(req, res) => {
+    try {
+        const deletedBands = await Band.destroy({
+            where: {
+                band_id: req.params.id
+            }
+        })
+        res.status(200).json({
+            message: `Successfully deleted ${deletedBands} band(s)`
+        })
+    } catch (err) {
+        res.status(500).json(err)
+    }
+})
+
 module.exports = bands
