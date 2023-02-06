8c589b0b150c6b80114a35dca4dd41d3f531e0e3
Thomas Davidson <tomd1969@gmail.com>
2023-02-06 16:17:58 -0500

    added post route for bands

--- controllers/bands_controller.js
+++ controllers/bands_controller.js
@@ -11,12 +11,26 @@ bands.get('/', async(req, res) => {
     }
 })
 
+//Get a band by ID
 bands.get('/:id', async (req, res) => {
     try {
         const foundBand = await Band.findOne({
             where: { band_id: req.params.id }
         })
         res.status(200).json(foundBand)
+    } catch (error) {
+        res.status(500).json(error)
+    }
+})
+
+//Create a band
+bands.post('/', async(req, res) => {
+    try {
+        const newBand = await Band.create(req.body)
+        res.status(200).json({
+            message: 'Successfully inserted a new band',
+            data: newBand
+        })
     } catch (error) {
         res.status(500).json(error)
     }
