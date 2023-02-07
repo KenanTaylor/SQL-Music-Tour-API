6af74ca0781d3a63ef1d4535e25f35203c5be5d8
Thomas Davidson <tomd1969@gmail.com>
2023-02-07 18:43:45 -0500

    added band/set_time association

--- models/band.js
+++ models/band.js
@@ -9,11 +9,15 @@ module.exports = (sequelize, DataTypes) => {
      * This method is not a part of Sequelize lifecycle.
      * The `models/index` file will call this method automatically.
      */
-    static associate({ MeetGreet}) {
+    static associate({ MeetGreet, Set_Time }) {
       // meet and greets
       Band.hasMany(MeetGreet, {
         foreignKey: 'band_id',
         as: 'meet_greets'
+      })
+      Band.hasMany(Set_Time, {
+        foreignKey: 'band_id',
+        as: 'set_times'
       })
     }
   }
