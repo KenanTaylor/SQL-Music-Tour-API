5cb6cc05b0ac84404309ee9fcf5bf85eae862044
Thomas Davidson <tomd1969@gmail.com>
2023-02-07 18:49:21 -0500

    added Event/set_time association

--- models/event.js
+++ models/event.js
@@ -9,7 +9,7 @@ module.exports = (sequelize, DataTypes) => {
      * This method is not a part of Sequelize lifecycle.
      * The `models/index` file will call this method automatically.
      */
-    static associate({ Stage, Stage_Event, Meet_Greet }) {
+    static associate({ Stage, Stage_Event, Meet_Greet, Set_Time }) {
       // stages
       Event.belongsToMany(Stage, {
         foreignKey: 'event_id',
@@ -19,6 +19,10 @@ module.exports = (sequelize, DataTypes) => {
       Event.hasMany(Meet_Greet, {
         foreignKey: 'event_id',
         as: 'meet_greets'
+      })
+      Event.hasMany(Set_Time, {
+        foreignKey: 'event_id',
+        as: 'set_times'
       })
     }
   }
