2cf3c33d86ee90eb85bffbaf82caab2b332fdd70
Thomas Davidson <tomd1969@gmail.com>
2023-02-07 19:04:57 -0500

    Fixed issue with band

--- models/band.js
+++ models/band.js
@@ -9,9 +9,9 @@ module.exports = (sequelize, DataTypes) => {
      * This method is not a part of Sequelize lifecycle.
      * The `models/index` file will call this method automatically.
      */
-    static associate({ MeetGreet, Set_Time }) {
+    static associate({ Meet_Greet, Set_Time }) {
       // meet and greets
-      Band.hasMany(MeetGreet, {
+      Band.hasMany(Meet_Greet, {
         foreignKey: 'band_id',
         as: 'meet_greets'
       })
